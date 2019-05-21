eb08fedd0cd9743eafcbe09369d6caa889d37833
Chris Mendoza <realcsmendoza@gmail.com>
2019-05-21 15:20:44 -0700

    restTemplate bean created in app main, autowired and used in HomeController

--- src/main/java/com/chriscodes/playground/HomeController.java
+++ src/main/java/com/chriscodes/playground/HomeController.java
@@ -3,18 +3,21 @@ package com.chriscodes.playground;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import com.chriscodes.playground.POJO.Capsule;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 public class HomeController {
-
+    
+    @Autowired
+    private RestTemplate restTemplate;
+    
     @GetMapping("/home")
     public String getHomepage(Model model) {
 
-        RestTemplate restTemplate = new RestTemplate();
+//        RestTemplate restTemplate = new RestTemplate();
         final String singleCapsuleUrl = "https://api.spacexdata.com/v3/capsules/{capsule_serial}";
         String capsuleID = "C112";
         
--- src/main/java/com/chriscodes/playground/PlaygroundApplication.java
+++ src/main/java/com/chriscodes/playground/PlaygroundApplication.java
@@ -2,15 +2,21 @@ package com.chriscodes.playground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
 public class PlaygroundApplication {
 
-	public static void main(String[] args) {
+    // used in application and during testing
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    
+    public static void main(String[] args) {
 
-		SpringApplication.run(PlaygroundApplication.class, args);
-	}
+            SpringApplication.run(PlaygroundApplication.class, args);
+    }
 }
