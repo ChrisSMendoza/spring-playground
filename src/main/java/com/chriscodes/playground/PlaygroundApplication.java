11ec21618387a0fdf6ecf44101ec665ac6600e8d
Chris Mendoza <realcsmendoza@gmail.com>
2019-05-21 12:54:01 -0700

    a single controller is in place for the home page

---
+++ src/main/java/com/chriscodes/playground/PlaygroundApplication.java
@@ -0,0 +1,17 @@
+package com.chriscodes.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class PlaygroundApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PlaygroundApplication.class, args);
+	}
+}
