0d069c646a5fc761c7c182aa1b428a5b505086c5
Chris Mendoza <realcsmendoza@gmail.com>
2019-05-21 13:11:13 -0700

    boot main uses @SpringBootApp, unit tests run correctly

--- src/main/java/com/chriscodes/playground/PlaygroundApplication.java
+++ src/main/java/com/chriscodes/playground/PlaygroundApplication.java
@@ -1,13 +1,12 @@
 package com.chriscodes.playground;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+
+@SpringBootApplication
 public class PlaygroundApplication {
 
 	public static void main(String[] args) {
